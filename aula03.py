8805577d68be40f710a5175294b7a83c96195495
André Luferat <contato@luferat.net>
2024-07-03 16:55:00 -0300

    Aula com Python

---
+++ aula03.py
@@ -0,0 +1,20 @@
+"""
+minha_var = "no meu país \"Biscoito\" não é bolacha"
+print(minha_var)
+
+if 5 == 2:
+ print("Five is greater than two!")  
+ if "a" == "a":
+      print('igualzinho')
+ 
+ print("Five is greater than two!")
+
+# else:
+#     print("Não é verdade")
+if 5 > 2:
+        print("Five is greater than two!") 
+"""
+x = 5
+y = "John"
+print(type(x))
+print(type(y))
